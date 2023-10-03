67cabecaaa13d7607cbb91f4f8a3f760bd31536d
Danijel-James Wynyard <danijel@repasscloud.com>
2023-10-04 00:46:40 +1100

    add countrylist service

---
+++ Services/CountryListService.cs
@@ -0,0 +1,29 @@
+﻿using AirAtlasPro.Services.Interfaces;
+
+namespace AirAtlasPro.Services
+{
+	public class CountryListService : ICountryListService
+	{
+        private readonly HttpClient _httpClient;
+
+		public CountryListService(HttpClient httpClient)
+		{
+            _httpClient = httpClient;
+		}
+
+        public IList<string> Countries { get; set; } = new List<string>();
+
+        public async Task LoadCountryListAsync()
+        {
+            if (Countries.Count == 0)
+            {
+                var countryList = await _httpClient.GetFromJsonAsync<IList<string>>("/api/CountryList");
+                if (countryList != null)
+                {
+                    Countries = countryList;
+                }
+            }
+        }
+    }
+}
+
